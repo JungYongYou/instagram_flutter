85b89b270c4ab0ba037510370662dc301d41a174
LUKARYU <fbfmzk1990>
2022-01-19 23:55:13 +0900

    flutter_svg 패키지 추가, assets에서 이미지 가져오기

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // svg image
+              SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
+              )
+              // text field input for email
+              // text field input for password
+              // button for login
+              // Transitioning to singing up
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
