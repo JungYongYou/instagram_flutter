a6854d55ca8157c5c72028b049fd0db384e4bf98
LUKARYU <fbfmzk1990>
2022-01-19 23:28:43 +0900

    firebase 앱 세팅, web 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyDCUd4KObZQvl7xUsqpCKLlDRhRTydCJUI',
+        appId: '1:133519155843:web:fe3f6d4c7e0349857ff1b6',
+        messagingSenderId: '133519155843',
+        projectId: 'instagram-flutter-a7dac',
+        storageBucket: 'instagram-flutter-a7dac.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
